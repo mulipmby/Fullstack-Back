4646dc1a9262ccc6236263e0873fb5ae17c1f393
mulipmby <paulus.muli@hotmail.com>
2023-02-14 11:48:21 +0200

    3.15, 3.16, 3.17, 3.18

--- index.js
+++ index.js
@@ -39,49 +39,51 @@ let persons = [
       "name": "Dan Abramov", 
       "number": "12-43-234345",
       "id": 3
-    },
-    { 
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122",
-      "id": 4
     }
+   
 ]
-
-  app.get('/persons', (request, response) => {
-    if (persons) {
+app.get('/persons', (request, response) => {
       Person.find({}).then(persons => {
         response.json(persons)
       })
-    } else {
-      response.status(404).end()
-    }
   })
 
   app.get('/info', (request, response) => {
-    var data = `phonebok has info for ${persons.length} people`
-    var time = new Date();
-    
-
-    if (persons) {
-      response.json(`${data} ${time}`)
-    } else {
-      response.status(404).end()
-    }
-  })
-
-  app.get('/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
+    Person.find({}).then(persons => {
+      response.json(`phonebook has info for ${persons.length} people ${Date()}`)
     })
-      
   })
 
-  app.delete('/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-  
-    response.status(204).end()
+  app.get('/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
   })
+
+  app.delete('/persons/:id', (request, response, next) => {
+    const id = request.params.id
+  
+    Person.findByIdAndDelete(id)
+      .then(() => { 
+        response.status(204).end()
+      })
+      .catch(error => next(error))   
+  })
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+  }
+  
+  // tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+  app.use(errorHandler)
 
   const PORT = process.env.PORT
   app.listen(PORT, () => {
@@ -94,6 +96,21 @@ let persons = [
       : 0
     return maxId + 1
   }
+
+  app.put('/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      name: body.name,
+      number: body.number
+    }
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
 
   app.post('/persons', (request, response) => {
     const body = request.body
@@ -127,4 +144,5 @@ let persons = [
       person.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
   })
